738dfece2927d33f25d3c70e3adf598ce9d08c97
jorge_ruiz <jorge.ruiz@logixsdigital.com>
2024-09-24 01:59:27 +0200

    delete credentials

--- src/config/Database.js
+++ src/config/Database.js
@@ -1,11 +1,11 @@
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-  "db-inventory",
-  "admin",
-  "Finibus_8onorum()etMalorum",
+  "db_name",
+  "user",
+  "password",
   {
-    host: "db-inventory.cdiaeo4es7qa.us-east-2.rds.amazonaws.com",
+    host: "localhost",
     dialect: "mysql",
   }
 );
